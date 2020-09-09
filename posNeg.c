ef7a314572b85aa076d5ed2d53a5be93e2d80ccb
Isaiah Gathala <55990958+isaiahgman@users.noreply.github.com>
2020-09-09 09:59:06 -0400

    Update posNeg.c

--- posNeg.c
+++ posNeg.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 
 int main(){
-	
+	//intialize
 	int num;
-
+	//takes input
 	printf("Enter a number: ");
 	scanf("%d", &num);
-
-	if(num < 0){
-		printf("Your number is negative.\n");
-	}
-	else if(num > 0){
-		printf("Your number is positive.\n");
-	}
-	else{
-		printf("Your number is neither negative or positive.\n");
-	}
+	//compares input to zero to determine sign
+	if(num < 0){ printf("Your number is negative.\n"); }
+	else if(num > 0){ printf("Your number is positive.\n"); }
+	else{ printf("Your number is neither negative or positive.\n"); }
 }
